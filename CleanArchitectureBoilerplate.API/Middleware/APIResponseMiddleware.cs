ff52caeb68af564a601075518b22d2817b8e55fd
Rob-Black-III <rdblack3@gmail.com>
2022-12-25 15:37:16 -0500

    Fixed APIResponseWrapper.

--- CleanArchitectureBoilerplate.API/Middleware/APIResponseMiddleware.cs
+++ CleanArchitectureBoilerplate.API/Middleware/APIResponseMiddleware.cs
@@ -19,6 +19,8 @@ namespace CleanArchitectureBoilerplate.API.Middleware
         (HttpContext context, ICleanArchitectureBoilerplateLogger logger, 
         IAPIResponseService responseService, IErrorService errorService)
         {
+            // Loosely stolen from:
+            // https://github.com/nayanbunny/dotnet-webapi-response-wrapper-sample/blob/main/DotNet.ResponseWrapper.Sample.Api/Middleware/ResponseWrapperMiddleware.cs
 
             // REQUEST
             
@@ -27,6 +29,13 @@ namespace CleanArchitectureBoilerplate.API.Middleware
 
             // 1. Get traceID that was updated in a prior middleware.
             responseWrapper.traceID = context.TraceIdentifier;
+
+            // Storing Context Body Response
+            var currentBody = context.Response.Body;
+
+            // Using MemoryStream to hold Controller Response
+            using var memoryStream = new MemoryStream();
+            context.Response.Body = memoryStream;
 
             await _next(context);
 
@@ -37,10 +46,31 @@ namespace CleanArchitectureBoilerplate.API.Middleware
             responseWrapper.issues =  errorService.GetAllErrors();
 
             // Replace the entire result with the APIResponseWrapper
-            var apiResponseObject = JsonConvert.SerializeObject(responseWrapper);
-            context.Response.ContentType = "application/json";
-            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-            await context.Response.WriteAsync(apiResponseObject);
+            // var apiResponseObject = JsonConvert.SerializeObject(responseWrapper);
+            // context.Response.ContentType = "application/json";
+            // context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            // await context.Response.WriteAsync(apiResponseObject);
+
+            // Resetting Context Body Response
+            context.Response.Body = currentBody;
+
+            // Setting Memory Stream Position to Beginning
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            // Read Memory Stream data to the end
+            var readToEnd = new StreamReader(memoryStream).ReadToEnd();
+
+            // Deserializing Controller Response to an object
+            var result = JsonConvert.DeserializeObject(readToEnd);
+
+            // Copy existing payload to apiResponse.payload and map to HTTP response object.
+            responseWrapper.payload = result;
+            var response = responseWrapper;
+
+            // returing response to caller
+            await context.Response.WriteAsync(JsonConvert.SerializeObject(response));
+
+            
         }
     }
 }
